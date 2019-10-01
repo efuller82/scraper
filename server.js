8d1aefdc6e157db4a168b21b98155011f6f47660
Eric Fuller <efuller82@gmail.com>
2019-10-01 09:28:58 -0500

    website successfully scraped and saved to db

--- server.js
+++ server.js
@@ -1,30 +1,80 @@
+var express = require('express');
+var mongojs = require('mongojs');
+
 var cheerio = require('cheerio');
 var axios = require('axios');
 
-//Making a request via axios for the Sentinel's news page
-axios.get('https://www.dailysentinel.com').then(function (response) {
-    // Load the response into cheerio and save it to a variable
-    var $ = cheerio.load(response.data);
+// Initialize express
+var app = express();
 
-    // An empty array to save the data that we'll scrape
-    var results = [];
+// Database configuration
+var databaseUrl = 'sentinelScraper';
+var collections = ['scrapedData'];
 
-    // With cheerio, find each p-tag with the 'tnt-summary' class
-    // (i: iterator. element: the current element) 
-    $('p.tnt-summary').each(function (i, element) {
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on('error', function (error) {
+    console.log('Database Error:', error);
+});
 
-        // Save the text of the element in a 'title' variable
-        var title = $(element).text();
+// Main route 
+app.get('/', function (req, res) {
+    // ! confused about this
+    res.send('yoyo');
+});
 
-        // In the currently selected element, look at its child elements (its a-tags),
-        // then sae the values for any 'href' attributes that the child elements may have 
-        var link = $(element).children().attr('href');
+// Retrieve data from the db
+app.get('all', function (req, res) {
+    //Find all results from the scrapedData collection in the db
+    db.scrapedData.find({}, function (error, found) {
+        // Throw any errors to the console
+        if (error) {
+            console.log(error);
+        }
+        // If there are no errors, send the data to the browser as json
+        else {
+            res.json(found);
+        }
+    });
+});
 
-        // Save the results in an object that we'll push into the results array we defined earlier
-        results.push({
-            title: title,
-            link: link
+// Scrape data from one site and place it into the mongo db
+app.get('/scrape', function (req, res) {
+    // Make a request via axios for the news section of The Daily Sentinel
+    axios.get('https://www.dailysentinel.com').then(function (response) {
+        // Load the html body from axios into cheerio
+        var $ = cheerio.load(response.data);
+        // For each p element with tnt-summary class
+        $('h3.tnt-headline').each(function (i, element) {
+            // Save the text and href of each link enclosed in the current element
+            var title = $(element).text();
+            var link = $(element).children('a').attr('href');
+
+            // If this found element had both a title and a link 
+            if (title && link) {
+                // Insert the data in the scrapedData db
+                db.scrapedData.insert({
+                    title: title,
+                    link: link
+                },
+                    function (err, inserted) {
+                        if (err) {
+                            // Log the error if one is encountered during the query
+                            console.log(err);
+                        }
+                        else {
+                            // Otherwise, log the inserted data
+                            console.log(inserted);
+                        }
+                    });
+            }
         });
     });
-    console.log(results);
+    // Send a "Scrape Complete" message to the browser
+    res.send('Scrape Complete')
+});
+
+// Listen on port 3000
+app.listen(3000, function () {
+    console.log('App running on port 3000!')
 });
